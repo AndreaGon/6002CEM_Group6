f2808d4aba30e8e04408012cbcc337b537a9921e
Wan <p20012339@student.newinti.edu.my>
2023-05-10 22:21:02 +0800

    small changes on home page

--- lib/view/home/homepage.dart
+++ lib/view/home/homepage.dart
@@ -28,6 +28,8 @@ class _HomePageState extends State<HomePage> {
         ),
         child: Scaffold(
             backgroundColor: Colors.transparent,
+
+            //top bar with side menu and chat icon
             appBar: AppBar(
               title: null,
               backgroundColor: white,
@@ -37,50 +39,14 @@ class _HomePageState extends State<HomePage> {
                 Padding(
                     padding: EdgeInsets.only(right: 20.0),
                     child: GestureDetector(
-                      onTap: () {},
-                      child: Icon(
-                        Icons.chat_outlined,
-                      ),
+                      onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));},
+                      child: Icon(Icons.chat_outlined,),
                     )
                 ),
               ]
               ),
 
-
-            //   Padding(
-            //       padding: const EdgeInsets.all(8.0),
-            //       child: Container(
-            //         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            //         child: TextField(
-            //           // controller: _searchController,
-            //           decoration: InputDecoration(
-            //             hintText: 'Search...',
-            //             border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0),
-            //             )
-            //             // Add a clear button to the search bar
-            //             //suffixIcon: IconButton(
-            //             //icon: Icon(Icons.clear),
-            //             //onPressed: () => _searchController.clear(),
-            //           ),
-            //           // Add a search icon or button to the search bar
-            //           //prefixIcon: IconButton(
-            //           //   icon: Icon(Icons.search),
-            //           //   onPressed: () {
-            //           //     // Perform the search here
-            //           //   },
-            //           // ),
-            //
-            //         ),
-            //       ),
-            //     ),
-            //   ]
-            //
-            // ),
-
-
-
-
-            //Side Menu
+            //list tile of side menu
             drawer: Container(
               child: Drawer(
                   child: ListView(padding: const EdgeInsets.all(0), children: [
@@ -146,16 +112,206 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
 
+            //Page content
             body: Container(
               margin: EdgeInsets.all(25.0),
               child: Column(
-                //mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                Text("HOME", textAlign: TextAlign.left, style: TextStyle(fontSize: 36, color: darkbrown, fontWeight: FontWeight.bold)),
+                children: <Widget> [
 
-                //SizedBox(height: 10),
+                //Page title
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    child: Text("HOME", textAlign: TextAlign.left, style: TextStyle(fontSize: 36, color: darkbrown, fontWeight: FontWeight.bold)),
+                  ),
+                ),
 
-                Container(margin: EdgeInsets.all(10.0),height:2.0, width:160, color:darkbrown),
+                //Underline
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(margin: EdgeInsets.fromLTRB(0, 10, 0, 10), height:2.0, width:160, color:darkbrown, alignment: Alignment.centerLeft),
+                ),
+
+                //Search box
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+
+                    child: TextField(
+                      // controller: _searchController,
+                      decoration: InputDecoration(hintText: 'Search...', border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+
+                        //Search button
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.search),
+                          onPressed: (){}, //=> _searchController(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Center(
+                  child: MergeSemantics(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Card(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Material(
+                                  color: Colors.white,
+                                  elevation: 0,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Container(
+
+                                      decoration: BoxDecoration(image: DecorationImage(image: AssetImage("assets/applogo.png"))),
+                                    ),
+
+
+                                ),
+
+                              //   Padding(
+                              //       padding: EdgeInsets.fromLTRB(20, 10.0, 20.0, 20.0),
+                              //       child: Column(
+                              //         crossAxisAlignment: CrossAxisAlignment.stretch,
+                              //         children: <Widget>[
+                              //           TextField(
+                              //             decoration: InputDecoration(
+                              //               labelText: "Email",
+                              //               hintText: "example@mail.com",
+                              //             ),
+                              //             autofocus: true,
+                              //           ),
+                              //           TextField(
+                              //             decoration: InputDecoration(
+                              //               labelText: "Password",
+                              //             ),
+                              //             autofocus: true,
+                              //           ),
+                              //           SizedBox(
+                              //             width: double.infinity,
+                              //             height: 100,
+                              //             // child: new RaisedButton(
+                              //             //   color: Colors.blue,
+                              //             //   child: Text(
+                              //             //     "Sign In",
+                              //             //     style: TextStyle(
+                              //             //       color: Colors.white,
+                              //             //       fontFamily: 'Raleway',
+                              //             //       fontSize: 22.0,
+                              //             //     ),
+                              //             //   ),
+                              //             //   onPressed: () => print('Sign In'),
+                              //             // ),
+                              //           )
+                              //         ],
+                              //       )),
+                                )],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+
+
+                // Align(
+                //   alignment: Alignment.center,
+                //   child: Row(
+                //     mainAxisSize: MainAxisSize.max,
+                //     children: [
+                //       Card(
+                //           elevation: 0,
+                //           shape: RoundedRectangleBorder(
+                //               side: BorderSide(color: Theme.of(context).colorScheme.outline),
+                //               borderRadius: const BorderRadius.all(Radius.circular(12))
+                //           ),
+                //         child: Row(
+                //           mainAxisSize: MainAxisSize.max,
+                //           children: [
+                //             SizedBox(
+                //               //width: 200,
+                //               child: Center(child: Text('Outlined Card')),
+                //             )
+                //
+                //           ],
+                //
+                //           ),
+                //         ),
+                //
+                //
+                //
+                //
+                //
+                //
+                //     ],
+                //   ),
+                //
+                // ),
+
+                //Book List
+                // Row(
+                //   mainAxisSize: MainAxisSize.max,
+                //   children: <Widget> [
+                //     Container(
+                //       margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                //       decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.white),
+                //
+                //
+                //     )
+                //   ],
+                // )
+
+                // Align(
+                //   alignment: Alignment.center,
+                //     //background card
+                //     child: Row(
+                //         mainAxisSize: MainAxisSize.max,
+                //         children: <Widget> [
+                //           Container(
+                //               margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                //               decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.white),
+                //
+                //               // child: Align(
+                //               // alignment: Alignment.center,
+                //               //   child: Container(
+                //               //       width: 200,
+                //               //       decoration: const BoxDecoration(
+                //               //           image: DecorationImage(
+                //               //               image: AssetImage("assets/applogo.png"), fit: BoxFit.cover)
+                //               //       )
+                //               //   ),
+                //               // )
+                //
+                //
+                //
+                //           )
+                //         ]
+                //
+                //
+                //
+                //
+                //
+                //     )
+                //
+                //
+                //
+                //
+                //
+                // ),
+
+
+
+
+
 
                 ]
               )
