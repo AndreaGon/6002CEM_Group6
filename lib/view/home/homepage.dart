6a87e3b86287e525afbae4d8bea100630f17eac9
Wan <p20012339@student.newinti.edu.my>
2023-05-22 15:43:11 +0800

    home page done

--- lib/view/home/homepage.dart
+++ lib/view/home/homepage.dart
@@ -20,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
 
     BooksVM booksVM = BooksVM();
+    final BooksVM AllBooksViewModel = BooksVM();
 
     return Container(
         decoration: const BoxDecoration(
@@ -94,59 +95,69 @@ class _HomePageState extends State<HomePage> {
                     padding: const EdgeInsets.all(8),
                       itemCount:  snapshot.data?.docs.length,
                       itemBuilder: (context, index) {
+                        return Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Column(
+                                children: [
+                                  //Book cover
+                                  Container(
+                                      padding: const EdgeInsets.all(20),
+                                      height: 330,
+                                      child: FutureBuilder<String>(
+                                          future: booksVM.getImage(snapshot.data
+                                              ?.docs[index]["book_cover"]),
+                                          builder: (context, snapshot) {
+                                            if (snapshot.hasData) {
+                                              return Container(
+                                                  decoration: BoxDecoration(
+                                                      borderRadius: BorderRadius
+                                                          .circular(20),
+                                                      image: DecorationImage(
+                                                          fit: BoxFit.cover,
+                                                          image: NetworkImage(
+                                                              snapshot.data ??
+                                                                  ""))));
+                                            }
 
-                      return Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Column(
-                              children: [
-                                //Book cover
-                                Container(
-                                    padding: const EdgeInsets.all(20),
-                                    height: 330,
-                                    child: FutureBuilder<String>(
-                                      future: AllBooksViewModel.getImage(streamSnapshot.data?.docs[index]["book_cover"]),
-                                      builder: (context, snapshot) {
-                                        if(snapshot.hasData){
-                                          return Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
-                                          image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(snapshot.data ?? ""))));
-                                        }
-
-                                        return CircularProgressIndicator();
-                                      }
-                                    )
+                                            return CircularProgressIndicator();
+                                          }
+                                      )
                                   ),
 
-                                //Book name and price
-                                ListTile(
-                                  onTap: () {
-                                    Navigator.push(context,
-                                        MaterialPageRoute(builder: (context) => BookInfo(snapshot.data.docs[index]['id'])));
+                                  //Book name and price
+                                  ListTile(
+                                    onTap: () {
+                                      Navigator.push(context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  BookInfo(snapshot.data
+                                                      .docs[index]['id'])));
                                     },
-                                  title: Text(snapshot.data.docs[index]['name']),
-                                  subtitle: Text(
-                                      'RM' + snapshot.data.docs[index]['price']),
-                                  trailing: Icon(Icons.arrow_forward_ios),
-                )
-                              ]
-                          )
-                      )
+                                    title: Text(
+                                        snapshot.data.docs[index]['name']),
+                                    subtitle: Text(
+                                        'RM' +
+                                            snapshot.data.docs[index]['price']),
+                                    trailing: Icon(Icons.arrow_forward_ios),
+                                  )
+                                ]
+                            )
+                        );
 
-                      // AllBooksViewModel.getImage(streamSnapshot.data?.docs[index]["book_cover"]).whenComplete(() async {
-                      //   String bookCoverUrl = "";
-                      //   bookCoverUrl = await AllBooksViewModel.getImage(streamSnapshot.data?.docs[index]["book_cover"]);
-                      //
-                      //   print(bookCoverUrl);
-                      // });
+                        // AllBooksViewModel.getImage(streamSnapshot.data?.docs[index]["book_cover"]).whenComplete(() async {
+                        //   String bookCoverUrl = "";
+                        //   bookCoverUrl = await AllBooksViewModel.getImage(streamSnapshot.data?.docs[index]["book_cover"]);
+                        //
+                        //   print(bookCoverUrl);
+                        // });
 
 
-
-
+                      }
                   )
                   );
                 }
-                )
-                ]
+                )]
             )
         )
         )
