b016e58e17603d6687639734cb95ec23c7071a1f
Wan <p20012339@student.newinti.edu.my>
2023-05-23 00:03:39 +0800

    book info data can pass, but Listview layout problem

--- lib/view_model/bookinfo_viewmodel.dart
+++ lib/view_model/bookinfo_viewmodel.dart
@@ -2,9 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BookInfoVM {
 
-  Future getBookInfo(String Id) async {
-    QuerySnapshot querySnapshot= await FirebaseFirestore.instance.collection("books").where("id", isEqualTo: Id).get();
-    return querySnapshot.docs[0].data();
+  Future getBookInfo(String bookId) async {
+    try {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection("books").where("id", isEqualTo: bookId).get();
+    if (querySnapshot.docs.isNotEmpty) {
+      return querySnapshot.docs[0].data();  }
+    } catch (e) {
+      throw Exception('Error occurred while fetching book information: $e');
+    }
   }
 
 }
